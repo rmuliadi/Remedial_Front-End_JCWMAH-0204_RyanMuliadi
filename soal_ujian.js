38e404316c2e04ca3b60893cc73db27cb8fc6ed5
rmuliadi <rmuliadi@users.noreply.github.com>
2021-02-28 15:56:28 +0700

    Add files via upload

---
+++ soal_ujian.js
@@ -0,0 +1,37 @@
+// Bikin Layout youtube home dan layout
+// Bikin navbar logo tanpa text, sidebar
+// 1. Bikin design layout youtube yang terdiri dari bbrp bagian:
+// a. Homepage
+// - Header(Logo, search, icons)
+// - Daftar Videos
+// b. Explore
+// - Daftar Videos
+
+// React
+// 1. FAKE JSON API -> buatlah 10 - 15 dummy data
+// Buatlah dummy data yang terdiri dari data:
+// {
+// image_url,
+// profile_pict_url,
+// title,
+// channel_name,
+// views,
+// created -> 7 years ago,
+// description -> tampilkan sebagian karakter saja
+// }
+
+// 2. Hit Fake JSON API yang kalian buat, dan tampilkan di layout timeline dan explore yang sebelumnya kalian buat.
+// Gunakan hal - hal berikut:
+// - Axios
+// - Mounting/ComponentWillMount
+// - State
+// - Props
+// - .map
+// - Route
+// - Link
+
+// Submit:
+// Paling lama 27 Feb 2021 Jam 4:15 sore.
+// Buatlah private repository Remedial_Front-End_JCWMAH-0204_{nama}
+// invite vincentwardhana33@gmail.com sebagai collaborator
+// kirim link private repo kalian ke operational @purwadhika.com
